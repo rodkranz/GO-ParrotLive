5f3d663a4a3c1cfd978fc7eb240165d51c9e82e9
Rodrigo Lopes <rodrigo.lopes@olx.com>
2018-04-23 17:58:29 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# GO-ParrotLive 
+
+An initiative, using frames from [`parrot-live`](https://github.com/hugomd/parrot.live), to allow any computer with `curl` to display an _animated_ party parrot.
+
+
+# Dependency
+```bash
+go get -u github.com/golang/dep
+```
+
+
+# Run!
+```bash
+go run main.go
+```
+
+
+# Try it!
+```bash
+curl localhost:8080
+```
+
+# More Parrots
+* [cultofthepartyparrot.com](http://cultofthepartyparrot.com/)
+* [`terminal-parrot`](https://github.com/jmhobbs/terminal-parrot)
+* [`parrotsay`](https://github.com/matheuss/parrotsay)
+* [`parrotsay`](https://github.com/matheuss/parrotsay)
+* [`parrot.live`](https://github.com/hugomd/parrot.live)
---
+++ main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"os"
+	"io"
+	"sync"
+	"math/rand"
+	"time"
+	"github.com/fatih/color"
+	"io/ioutil"
+	"path/filepath"
+	"net/http"
+	"strings"
+	"log"
+)
+
+type fnColors func(w io.Writer, format string, a ...interface{})
+
+type CarrotLive struct {
+	mutex  sync.RWMutex
+	Frames []string
+	index  int
+	
+	colors []fnColors
+}
+
+func init() {
+	rand.Seed(time.Now().UTC().UnixNano())
+}
+
+func New(p string) (*CarrotLive, error) {
+	carrot := &CarrotLive{
+		colors: []fnColors{
+			color.New(color.FgRed).FprintfFunc(),
+			color.New(color.FgYellow).FprintfFunc(),
+			color.New(color.FgGreen).FprintfFunc(),
+			color.New(color.FgBlue).FprintfFunc(),
+			color.New(color.FgMagenta).FprintfFunc(),
+			color.New(color.FgCyan).FprintfFunc(),
+			color.New(color.FgWhite).FprintfFunc(),
+		},
+	}
+	
+	if err := carrot.readFiles(p); err != nil {
+		return nil, err
+	}
+	
+	return carrot, nil
+}
+
+func (c *CarrotLive) readFiles(p string) (error) {
+	fnRead := func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+		
+		bs, err := ioutil.ReadFile(path)
+		if err != nil {
+			return err
+		}
+		
+		c.Frames = append(c.Frames, string(bs))
+		return nil
+	}
+	
+	return filepath.Walk(p, fnRead)
+}
+
+func (c *CarrotLive) NextFrame() string {
+	defer c.mutex.RUnlock()
+	c.mutex.RLock()
+	c.index = c.index + 1
+	
+	if len(c.Frames) <= c.index {
+		c.index = 0
+	}
+	
+	return c.Frames[c.index]
+}
+
+func (c *CarrotLive) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	if !strings.Contains(req.Header.Get("user-agent"), "curl") {
+		http.Redirect(rw, req, "https://github.com/hugomd/parrot.live", http.StatusFound)
+		return
+	}
+	
+	hj, ok := rw.(http.Hijacker)
+	if !ok {
+		http.Error(rw, "webserver doesn't support hijacking", http.StatusInternalServerError)
+		return
+	}
+	
+	conn, bufrw, err := hj.Hijack()
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	
+	defer conn.Close()
+	
+	for {
+		select {
+		case <-time.After(70 * time.Millisecond):
+			c.colors[rand.Intn(len(c.colors))](bufrw, "\033[2J\033[H")
+			c.colors[rand.Intn(len(c.colors))](bufrw, c.NextFrame())
+			c.colors[rand.Intn(len(c.colors))](bufrw, "\nWelcome PARROT LIVE Mother fucker!!!\n")
+			bufrw.Flush()
+		}
+	}
+}
+
+func main() {
+	c, err := New("./frames")
+	if err != nil {
+		log.Fatalf("Error to read frames because: %v", err)
+		os.Exit(1)
+	}
+	
+	if err = http.ListenAndServe(":8080", c); err != nil {
+		log.Fatalf("Error to start server because: %v", err)
+		os.Exit(1)
+	}
+}
